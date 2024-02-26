4f8e52dedfae37b3e05895fa522b77dca20772fd
Ricky Wong <rickywong0625@gmail.com>
2024-02-26 17:56:40 +0800

    feat: wordpress container installation done
    - install wp-cli and install wordpress using it
    - created a script to simplify the process
    - linked /var/www/html to host's volume so that nginx can link to it

---
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# should run this script in /var/html/www. this is where wp-config.php will be located.
+
+WP_CONFIG="./wp-config.php"
+
+if [ -e "$WP_CONFIG" ]; then
+    exit 1
+fi
+
+# fetches necessary files for wordpress
+wp core download --allow-root
+
+# create a wp-config.php with the specified database details
+wp config create \
+    --dbname="$WP_DB_NAME" \
+    --dbuser="$WP_DB_USER" \
+    --dbpass="$WP_DB_PASS" \
+    --dbhost="$WP_DB_HOST" \
+    --allow-root
+
+# install wordpress and create admin account
+wp core install \
+    --url="$DOMAIN_NAME" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASS" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+
+# create user account
+wp user create \
+    "$WP_USER_USER" \
+    "$WP_USER_EMAIL" \
+    --user_pass="$WP_USER_PASS" \
+    --first_name="$WP_USER_FNAME" \
+    --last_name="$WP_USER_LNAME" \
+    --allow-root
+
+# need this otherwise cannot run :)
+mkdir -p /run/php/
+
+# pass over to another command
+exec "$@"
